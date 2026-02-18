8b76a69762da42adef002e1ee838912de03987e7
io <tzwlink@163.com>
2026-02-18 14:56:06 +0800

    fix(project): 修复 react 被重复打包问题

--- vite.config.ts
+++ vite.config.ts
@@ -20,16 +20,14 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          // 把 React 和 ReactDOM 单独打包到 react-vendor chunk
-          if (id.includes("node_modules/react") || id.includes("node_modules/react-dom")) {
-            return "react";
-          }
-        },
-        // 控制 chunk 文件名
-        chunkFileNames: () => {
-          return 'assets/[name]-[hash].js';
-        },
+        // manualChunks(id) {
+        //   if (id.includes("node_modules")) {
+        //     return "node_modules";
+        //   }
+        // },
+        // chunkFileNames: () => {
+        //   return 'assets/[name]-[hash].js';
+        // },
       },
     },
   }
